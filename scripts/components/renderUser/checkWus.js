27ac0ec7f875cdf04d59bb76738f354c01cf9c28
Heanzy Zabala <heanzy.zabala@gmail.com>
2021-03-31 14:36:17 +0800

    Uncomment lines

--- scripts/components/renderUser/checkWus.js
+++ scripts/components/renderUser/checkWus.js
@@ -7,27 +7,27 @@ export const checkYourWus = (dataBanano, dataFold) => {
 
   //Verify if the next day from the last work unit has payment, AND
   //Verify if the next day from last work unit done is bigger than today
-  // if (
-  //   nextDayFromTheLastWorkUnit >= paydayTime &&
-  //   todayAtMidnight <= nextDayFromTheLastWorkUnit &&
-  //   lastWorkUnit
-  // ) {
-  //   localStorage.setItem("wu-active", true);
-  //   return `
-  //   <section class="banano__info">
-  //   <p class="banano__info--status">Account Status: active</p>
-  //   </section>`;
-  // } else {
-  //   localStorage.setItem("wu-active", false);
-  //   return `
-  //   <section class="banano__suspended">
-  //   <section class="banano__suspended--info">
-  //     <p>It seems your bananominer ID is inactive due to lack of recent folding activity.<br>
-  //     Please re-enter your ban_ address on <a href="http://www.bananominer.com" target="_blank" title="click here to visit">bananominer.com</a></p>
-  //   </section>
-  //   </section>
-  //   `;
-  // }
+  if (
+    nextDayFromTheLastWorkUnit >= paydayTime &&
+    todayAtMidnight <= nextDayFromTheLastWorkUnit &&
+    lastWorkUnit
+  ) {
+    localStorage.setItem("wu-active", true);
+    return `
+    <section class="banano__info">
+    <p class="banano__info--status">Account Status: active</p>
+    </section>`;
+  } else {
+    localStorage.setItem("wu-active", false);
+    return `
+    <section class="banano__suspended">
+    <section class="banano__suspended--info">
+      <p>It seems your bananominer ID is inactive due to lack of recent folding activity.<br>
+      Please re-enter your ban_ address on <a href="http://www.bananominer.com" target="_blank" title="click here to visit">bananominer.com</a></p>
+    </section>
+    </section>
+    `;
+  }
 };
 
 const todayDate = () => {
